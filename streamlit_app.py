adade6af3642cd13d84e1e7dea21d8f8c1460628
KinsukhS <128243518+KinsukhS@users.noreply.github.com>
2023-08-03 21:38:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title('My first app')
-
+st.info("hello")
 st.write("Here's our first attempt at using data to create a table:")
 st.write("hello there can you see this?")
